847042678f6aa6769f8c183af87c0b80cfae1c38
Leonardo Pereira <leonardopereirajr@gmail.com>
2025-02-22 19:01:13 -0300

    hotfix/servico-salvamento
    Corrigido injecao de dependencia

--- src/main/java/com/gh/backend/gh/repository/ServicoRepository.java
+++ src/main/java/com/gh/backend/gh/repository/ServicoRepository.java
@@ -2,6 +2,8 @@ package com.gh.backend.gh.repository;
 
 import com.gh.backend.gh.model.Servico;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ServicoRepository extends MongoRepository<Servico, String> {
 }
